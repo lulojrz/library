347fb822269fc55536bf1037853c626b7eb7fcfa
lulojrz <lulojrz18@gmail.com>
2024-11-21 18:04:28 -0300

    primer adelanto

---
+++ README.md
@@ -0,0 +1,1 @@
+Es una biblioteca, la cual se puede ingresar libros para agregar mediante js, tambien hay que mostrarlos y dar la opcion de eliminarlos.
---
+++ script.js
@@ -0,0 +1,17 @@
+libros = [
+    {
+        nombre : "1984",
+        autor : "George Well",
+        año : "1970",
+        leido: false
+
+    },
+    {
+        nombre : "El diario de un chico en apuros",
+        autor:"Jaime Borala",
+        año : "1983",
+        leido : true
+    }
+]
+
+
